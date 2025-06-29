7f9b80e491cc2de0dc765b4c795f341a82bcde0c
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2025-06-29 14:40:48 +0430

    Finish result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,40 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Result = () => { 
 
+  const [image , setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded , setIsImageLoaded] = useState(false);
+  const [loading , setLoading] = useState(false);
+  const [input , setInput] = useState('');
+
+  const onSubmitHandler = async (e) => {
+
+
+
+  }
+
   return (
 
-    <form action="" className='flex flex-col min-h-[90vh] items-center justify-center'>
+    <form onSubmit={onSubmitHandler} action="" className='flex flex-col min-h-[90vh] items-center justify-center'>
 
       <div>
 
         <div className="relative">
 
-          <img src={assets.sample_img_1} alt="" className="max-w-sm rounded" />
-          <span className="absolute bottom-0 right-0 h-1 bg-blue-500 w-full transition-all duration-[10s]" />
+          <img src={image} alt="" className="max-w-sm rounded" />
+          <span className={`absolute bottom-0 right-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`} />
 
         </div>
 
-        <p>در حال بارگزاری...</p>
+        <p className={!loading ? 'hidden' : ''}>در حال بارگزاری...</p>
 
       </div>
 
-      <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full flex-row-reverse'>
+      {!isImageLoaded &&
 
-        <input type="text" placeholder='در اینجا تصویر موردنظر خود را توصیف کنید' className='flex-1 bg-transparent outline-none mr-8 max-sm:w-20' />
+        <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full flex-row-reverse'>
 
-        <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full' type='submit'>تولید تصویر</button>
+          <input onChange={e => setInput(e.target.value)} value={input}
+          type="text" placeholder='در اینجا تصویر موردنظر خود را توصیف کنید' className='flex-1 bg-transparent outline-none mr-8 max-sm:w-20' />
 
-      </div>
+          <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full' type='submit'>تولید تصویر</button>
 
-      <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full flex-row-reverse'>
+        </div>
 
-        <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>تولید دوباره تصویر</p>
+      }
 
-        <a href="" download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>دانلود</a>
+      {isImageLoaded &&
 
-      </div>
+        <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full flex-row-reverse'>
+
+          <p onClick={() => {setIsImageLoaded(false)}} className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>تولید دوباره تصویر</p>
+
+          <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>دانلود</a>
+
+        </div>
+
+      }
 
     </form>
 
